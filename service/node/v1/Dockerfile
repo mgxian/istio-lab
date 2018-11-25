903c55f723920a40949946eb0444d425851b37b2
will <maoguangxian@famulei.com>
2018-11-25 17:31:04 +0800

    feat: add service-node code and Dockerfile

---
+++ service/node/v1/Dockerfile
@@ -0,0 +1,7 @@
+FROM node:8-alpine
+LABEL maintainer="will835559313@163.com"
+COPY . /app
+WORKDIR /app
+RUN npm i
+EXPOSE 80
+CMD ["node", "main.js"]
---
+++ service/node/v1/main.js
@@ -0,0 +1,59 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const axios = require('axios')
+const app = new Koa();
+const router = new Router();
+
+
+function getForwardHeaders(request) {
+    headers = {}
+    incoming_headers = [
+        'x-request-id',
+        'x-b3-traceid',
+        'x-b3-spanid',
+        'x-b3-parentspanid',
+        'x-b3-sampled',
+        'x-b3-flags',
+        'x-ot-span-context'
+    ]
+
+    for (idx in incoming_headers) {
+        ihdr = incoming_headers[idx]
+        val = request.headers[ihdr]
+        if (val !== undefined && val !== '') {
+            headers[ihdr] = val
+            console.log("incoming: " + ihdr + ":" + val)
+        }
+    }
+    return headers
+}
+
+
+
+router.get('/status', async (ctx, next) => {
+    ctx.body = 'ok';
+})
+
+router.get('/env', async (ctx, next) => {
+    forwardHeaders = getForwardHeaders(ctx.request)
+    service_go_url = 'http://' + 'service-go' + '/env'
+    try {
+        // console.log(forwardHeaders)
+        // upstream_ret = JSON.parse('{"message": "go v1"}')
+        const response = await axios.get(service_go_url, {
+            headers: forwardHeaders
+        });
+        upstream_ret = response.data
+        console.log(upstream_ret);
+        const version = process.versions.node;
+        ctx.body = {
+            'message': 'node' + version,
+            'upstream': upstream_ret
+        };
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(80);

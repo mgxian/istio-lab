b6d835d506de2c16d671593de3099ec8fc1bf0ef
will <maoguangxian@famulei.com>
2018-11-25 21:47:20 +0800

    feat: vue use vue-echarts to show service call

--- service/js/v2/src/components/HelloWorld.vue
+++ service/js/v2/src/components/HelloWorld.vue
@@ -2,37 +2,143 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button @click="handleClick">发射</button>
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
+    <div id="service-graph">
+      <v-chart ref="chart1" :options="orgOptions" :auto-resize="true"></v-chart>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ECharts from 'vue-echarts'
+// import 'echarts/lib/chart/graph'
+
 export default {
   name: 'HelloWorld',
-  data () {
+  components: {
+    'v-chart': ECharts
+  },
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App',
-      message: ''
+      message: '',
+      orgOptions: {},
+      serviceNodes: [],
+      serviceLinks: [],
+      serviceCall: {
+        message: 'python-python',
+        upstream: [
+          {
+            message: 'lua'
+          },
+          {
+            message: 'node',
+            upstream: [
+              {
+                message: 'go'
+              }
+            ]
+          }
+        ]
+      },
+      firstNodePos: [300, 300]
     }
   },
   methods: {
-    handleClick () {
+    handleClick() {
       let url = 'http://istio-test.will/env'
       axios.defaults.headers.common['App-Client'] = 'vue'
-      axios.get(url).then(res => {
-        this.message = 'vue----->' + res.data.message
-      }).catch(error => {
-        console.log(error)
-      })
+      axios
+        .get(url)
+        .then(res => {
+          this.message = 'vue -----> ' + res.data.message
+          this.serviceCall = res.data
+          this.setChar()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      // this.setChar()
+    },
+    parseServiceCall(data) {
+      if (data.upstream) {
+        data.upstream.forEach(upstream => {
+          let message = this.parseServiceCall(upstream)
+          let link = { source: data.message, target: message }
+          this.serviceLinks.push(link)
+        })
+      }
+
+      let node = { name: data.message }
+      this.serviceNodes.push(node)
+
+      return data.message
+    },
+    getServiceNodesAndLinks() {
+      this.serviceNodes = []
+      this.serviceLinks = []
+      let ret = this.parseServiceCall(this.serviceCall)
+      let node = { name: 'vue' }
+      let link = {
+        source: 'vue',
+        target: ret
+      }
+      this.serviceNodes.push(node)
+      this.serviceLinks.push(link)
+    },
+    setChar() {
+      this.getServiceNodesAndLinks()
+      // console.log(this.serviceNodes, this.serviceLinks)
+      this.orgOptions = {
+        series: [
+          {
+            type: 'graph',
+            layout: 'circular',
+            focusNodeAdjacency: true,
+            symbol: 'circle',
+            symbolSize: 90,
+            roam: false,
+            label: {
+              normal: {
+                show: true,
+                textStyle: {
+                  fontSize: 16
+                }
+              }
+            },
+            color: '#41B783',
+            edgeSymbol: ['circle', 'arrow'],
+            edgeSymbolSize: [4, 15],
+            edgeLabel: {
+              normal: {
+                textStyle: {
+                  fontSize: 15
+                }
+              }
+            },
+            data: this.serviceNodes,
+            links: this.serviceLinks,
+            lineStyle: {
+              normal: {
+                opacity: 0.9,
+                width: 3,
+                curveness: 0
+              }
+            }
+          }
+        ]
+      }
     }
-  }
+  },
+  mounted() {}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -45,5 +151,14 @@ li {
 }
 a {
   color: #42b983;
+}
+
+#service-graph {
+  margin: auto auto;
+  padding: auto auto;
+}
+
+.echarts {
+  width: 100%;
 }
 </style>
--- service/js/v2/src/main.js
+++ service/js/v2/src/main.js
@@ -8,6 +8,8 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })

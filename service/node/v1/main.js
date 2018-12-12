2a04893876ffcd6272e991fe968aff0fc734f192
will <maoguangxian@famulei.com>
2018-12-12 23:08:17 +0800

    add fault injection

--- service/node/v1/main.js
+++ service/node/v1/main.js
@@ -38,7 +38,7 @@ router.get('/env', async (ctx, next) => {
     forwardHeaders = getForwardHeaders(ctx.request)
     // service_go_url = 'http://httpbin.org/delay/3'
     service_go_url = 'http://' + 'service-go' + '/env'
-    upstream_ret = {}
+    upstream_ret = null
     try {
         // console.log(forwardHeaders)
         // upstream_ret = JSON.parse('{"message": "go v1"}')
